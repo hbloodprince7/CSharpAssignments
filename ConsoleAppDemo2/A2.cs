7d9abaf241e46a0321dcb4ac6b1e33d681cb581f
hbloodprince7 <manvithpshetty@gmail.com>
2022-08-26 15:03:11 +0530

    Add project files.

---
+++ ConsoleAppDemo2/A2.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleAppDemo2
+{
+    public class A2
+    {
+        public static int SmallestNum(int a, int b)
+        {
+            if (a > b)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
+
+        }
+        public static void main()
+        {
+            int a, b;
+            Console.WriteLine("Enter the numbers");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+        }
+
+    }
+
+}
